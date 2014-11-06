1d640010bb664940e1de74a0d5973653a0e69a52
Peter Manev <pmanev@stamus-networks.com>
2014-11-06 16:05:17 -0500

    Restart ES and LG only after successful Java install

--- Scripts/Java/setup-oracle-java_stamus.sh
+++ Scripts/Java/setup-oracle-java_stamus.sh
@@ -18,20 +18,6 @@ echo -e "##########################"
 echo -e "# SETTING UP ORACLE JAVA #"
 echo -e "##########################"
 
-if ps aux | grep -v grep | grep elasticsearch |grep java > /dev/null 
-then
-    echo "Elasticsearch running - stopping before Java install!"
-    ES_WAS_RUNNING=0
-    /etc/init.d/elasticsearch stop
-fi
-
-if ps aux | grep -v grep | grep logstash |grep java > /dev/null 
-then
-    echo "Logstash running - stopping before Java install!"
-    LG_WAS_RUNNING=0
-    /etc/init.d/logstash stop
-fi
-
 
 echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee /etc/apt/sources.list.d/webupd8team-java.list
 echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
@@ -40,29 +26,17 @@ apt-get update
 
 # to automatically accept the ORACLE license (no user input needed)
 #echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-apt-get install  oracle-java7-installer oracle-java7-set-default
+apt-get install oracle-java7-installer 
+apt-get install oracle-java7-set-default
 
-if [ $? != 0 ]; 
-then
-    # continue with the script if the user has chosen not to install
-    exit 0
-fi
 
-if [ ${ES_WAS_RUNNING} -eq 0 ]; 
-then
-    echo "Starting up previously stopped Elasticsearch service after Java upgrade!"
-    /etc/init.d/elasticsearch start
-    ES_WAS_RUNNING=999
-fi
+echo "Restarting Elasticsearch service after Java upgrade!"
+/etc/init.d/elasticsearch restart
 
 sleep 5
 
-if [ ${LG_WAS_RUNNING} -eq 0 ]; 
-then
-    echo "Starting up previously stopped Logstash service after Java upgrade!"
-    /etc/init.d/logstash start
-    LG_WAS_RUNNING=999
-fi
+echo "Restarting Logstash service after Java upgrade!"
+/etc/init.d/logstash restart
 
 echo -e "###############################"
 echo -e "# RUNNING ORACLE JAVA VERSION # ==>"
