f3c2c96898fc538178769f69148ea22d86004591
Peter Manev <pmanev@stamus-networks.com>
2016-09-08 15:04:35 +0200

    selks: add an upgrade wrapper script
    https://github.com/StamusNetworks/SELKS/wiki/SELKS-upgrades

---
+++ Scripts/Setup/selks-upgrade_stamus.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Copyright(C) 2014, Stamus Networks
+# All rights reserved
+# Part of Debian SELKS scripts
+# Written by Peter Manev <pmanev@stamus-networks.com>
+#
+# Please run on Debian
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+/bin/systemctl stop kibana
+/usr/share/elasticsearch/bin/plugin remove delete-by-query
+
+apt-get update && apt-get dist-upgrade
+
+chown -R kibana /opt/kibana/optimize/
+/usr/share/elasticsearch/bin/plugin install delete-by-query
+
+/bin/systemctl restart elasticsearch
+/bin/systemctl restart kibana
+
