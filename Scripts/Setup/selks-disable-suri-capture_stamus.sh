54ef61f4c0c6fe9fe7dcd5da5ea91fdf0c28215c
Peter Manev <pmanev@stamus-networks.com>
2018-12-06 11:08:35 +0100

    setup: make sure we explicitly disable or enable FPC with Suricata

---
+++ Scripts/Setup/selks-disable-suri-capture_stamus.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Copyright(C) 2018, Stamus Networks
+# All rights reserved
+# Part of Debian SELKS scripts
+# Written by Peter Manev <pmanev@stamus-networks.com>
+#
+# Please run on Debian
+#
+# This script comes with ABSOLUTELY NO WARRANTY!
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+sed -i  '/- pcap-log:/{n;s/yes/no/}' /etc/suricata/selks5-addin.yaml
--- Scripts/Setup/selks-first-time-setup_stamus.sh
+++ Scripts/Setup/selks-first-time-setup_stamus.sh
@@ -55,6 +55,7 @@ do
         FPC) 
             echo "Enable Full Pcacket Capture"
             EXIT_STATUS="SUCCESS"
+            /opt/selks/Scripts/Setup/selks-enable-suri-capture_stamus.sh && \
             /opt/selks/Scripts/Setup/selks-molochdb-init-setup_stamus.sh FPC
             if [ $? -ne 0 ]; then
               echo "Moloch set up job failed...Exiting..." 
@@ -65,6 +66,7 @@ do
         FPC_Retain) 
             echo "Enable Full Pcacket Capture with pcap retaining "
             EXIT_STATUS="SUCCESS"
+            /opt/selks/Scripts/Setup/selks-enable-suri-capture_stamus.sh && \
             /opt/selks/Scripts/Setup/selks-molochdb-init-setup_stamus.sh
             if [ $? -ne 0 ]; then
               echo "Moloch set up job failed...Exiting..." 
@@ -73,7 +75,14 @@ do
             fi
             break;;
         NONE)
+            echo "Disable Full Packet Capture "
             EXIT_STATUS="SUCCESS"
+            /opt/selks/Scripts/Setup/selks-disable-suri-capture_stamus.sh
+            if [ $? -ne 0 ]; then
+              echo "Moloch set up job failed...Exiting..." 
+              echo -e "\n### Exited with ERROR  ###\n" 
+              EXIT_STATUS="FAILED"
+            fi
             break;;
      esac
 done
